1f628b5b4e95fa572da679776fd78715b0b8838e
zhjm <943257156@qq.com>
2020-02-25 09:02:31 +0800

    '待付款到时间小于十加0'

--- src/pages/bill/index.vue
+++ src/pages/bill/index.vue
@@ -290,6 +290,9 @@ export default {
           if (h + m + s === 0) {
             item.status = '违约'
           } else {
+            h = h < 10 ? '0' + h : h
+            m = m < 10 ? '0' + m : m
+            s = s < 10 ? '0' + s : s
             item.timeDown = `${h}:${m}:${s}`
           }
         }
