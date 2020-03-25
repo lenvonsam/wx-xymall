b516b9191d0c48dd89b4ea3e1faadc9a07c5ce80
zhjm <943257156@qq.com>
2020-03-25 13:13:07 +0800

    '卖家购物车'

--- src/components/SellerCartTemp.vue
+++ src/components/SellerCartTemp.vue
@@ -571,15 +571,15 @@ export default {
             const prArr = []
             const wtArr = []
             const oldPrArr = []
-            if (Number(itm.bj_price) > 0) {
-              prArr.push(itm.bj_price)
-              wtArr.push(itm.bj_weight)
-              oldPrArr.push(itm.bj_origin_price)
-            }
             if (Number(itm.lj_price) > 0) {
               prArr.push(itm.lj_price)
               wtArr.push(itm.lj_weight)
               oldPrArr.push(itm.lj_origin_price)
+            }
+            if (Number(itm.bj_price) > 0) {
+              prArr.push(itm.bj_price)
+              wtArr.push(itm.bj_weight)
+              oldPrArr.push(itm.bj_origin_price)
             }
             if (Number(itm.lj_price16) > 0) {
               prArr.push(itm.lj_price16)
