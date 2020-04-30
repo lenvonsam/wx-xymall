11ad0694bc5bc008f38b42b706a14803008a76d8
zhjm <943257156@qq.com>
2020-04-30 15:43:59 +0800

    '购物车，个人中心未登录状态显示'

---
+++ src/components/NoData.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.content.bg-white.row(:style="{height: height+'rpx'}")
+  img.img(src="/static/images/person_icon.png")
+  .ft-16.margin-top-lg 您还没有登录哦~
+  .row.button-group.margin-top-sm
+    .col.text-right(@click="jump('/pages/account/login/main')") 登陆
+    span.pl-10.pr-10 /
+    .col(@click="jump('/pages/account/register/main')") 注册
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      height: 0
+    }
+  },
+  computed: {
+    ...mapState({
+      bottomBarHeight: state => state.bottomBarHeight,
+      isIpx: state => state.isIpx,
+      screenHeight: state => state.screenHeight,
+      customBar: state => state.customBar
+    })
+  },
+  beforeMount () {
+    this.height = this.getRpx(this.screenHeight) - this.getRpx(this.customBar) - this.getRpx(this.bottomBarHeight)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.content
+  flex-direction column
+  justify-content center
+.img
+  width 80.5px
+  height 82.5px
+.button-group
+  justify-content center
+  color #ffffff
+  background-color #2485FF
+  width 150px
+  height 35px
+  line-height 35px
+  border-radius 20px
+</style>
