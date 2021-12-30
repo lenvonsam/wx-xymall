2a7c9f8ca9d9a689c41af09c63707b699f0f5e4c
myforkwxd <1215368751@qq.com>
2021-12-30 14:17:24 +0800

    16698 【准上线】【型云小程序】使用微信登录无效

--- src/components/WxLogin.vue
+++ src/components/WxLogin.vue
@@ -15,24 +15,31 @@ export default {
   },
   data () {
     return {
-      bindwx: false,
       openId: '',
+      unionId: '',
       onpresscTime: 0 // 阻止短时间内连续点击
     }
   },
-  onLoad () {
-    // this.openId = mpvue.getStorageSync('openId')
-    // console.log('openId===>' + this.openId)
-    this.wxAuthLogin()
-  },
+  // onLoad () {
+  //   this.openId = mpvue.getStorageSync('openId')
+  //   console.log('openId===>' + this.openId)
+  //   if (typeof this.openId !== 'undefined' && this.openId) {
+  //     this.bindwx = true
+  //   }
+  // },
   methods: {
     ...mapActions([
       'setUser'
     ]),
-    // 微信小程序登陆
-    wxAuthLogin () {
-      const me = this
-      try {
+    bindWxBtn () {
+      console.log('bindWx++++')
+      let me = this
+      if ((Date.now() - this.onpresscTime) > 2000) {
+        this.onpresscTime = Date.now()
+        /** 判断是否已绑定微信（通过接口返回判断）
+       * 已绑定==》自动登录，跳转用户中心
+       * 未绑定==》跳转账号绑定页面
+       */
         const localOpenId = mpvue.getStorageSync('openId') || false
         if (!localOpenId) {
           mpvue.login({
@@ -45,8 +52,15 @@ export default {
                     key: 'openId',
                     data: data.openId
                   })
-                  me.bindwx = true
                 }
+                if (data.return_code === 0 && data.unionId) {
+                  me.unionId = data.unionId
+                  mpvue.setStorage({
+                    key: 'unionId',
+                    data: data.unionId
+                  })
+                }
+                me.login()
               }).catch(e => {
                 console.error('login fail', e)
               })
@@ -56,86 +70,50 @@ export default {
             }
           })
         } else {
-          me.openId = mpvue.getStorageSync('openId')
-        }
-      } catch (e) {
-        console.error('get storage info error:>>', e)
-      }
-    },
-    bindWxBtn () {
-      console.log('aaaa++++')
-      console.log('self.bindwx===>' + this.bindwx)
-      let self = this
-      if ((Date.now() - this.onpresscTime) > 2000) {
-        this.onpresscTime = Date.now()
-        /** 判断是否已绑定微信（通过接口返回判断）
-       * 已绑定==》自动登录，跳转用户中心
-       * 未绑定==》跳转账号绑定页面
-       */
-        if (self.bindwx) {
-          // 微信自动登录接口
-          let paramsObj = {
-            openId: self.openId,
-            unionId: mpvue.getStorageSync('unionId')
-          }
-          self.httpGet(self.apiList.zf.wxLogin, paramsObj).then(res => {
-            self.setUser({token: res.data.token, user: res.data.user})
-            self.httpPost(self.apiList.zf.getPersonInfo, {}).then(res => {
-              self.setUser({user: res.data})
-              if (res.data.userStatus === '01') {
-                self.confirm({ content: '您是新用户，请先完善公司信息' }).then(res => {
-                  if (res === 'confirm') {
-                    self.jump('/pages/account/companyUpdate/main')
-                  } else {
-                    self.tab('/pages/me/main')
-                  }
-                })
-              } else {
-                this.showMsg('登录成功')
-                setTimeout(function () {
-                  self.canClick = true
-                  if (self.backType === 1) {
-                    console.log('+++++++aaa')
-                    self.back()
-                  } else {
-                    console.log('+++++++bbb')
-                    self.tab('/pages/index/main')
-                  }
-                }, 500)
-              }
-            })
-          }).catch(e => {
-            console.log('微信未绑定过手机号，登陆失败——+++')
-            console.log(e)
-            self.jump('/pages/account/wxBind/main')
-          })
-
-          // self.ironRequest(this.apiList.xy.wxLogin.url, { openid: self.openId }, this.apiList.xy.wxLogin.method).then((res) => {
-          //   console.log('res===>' + JSON.stringify(res))
-          //   self.setUser(res)
-          //   self.showMsg(res.errormsg)
-          //   setTimeout(function () {
-          //     if (self.backType === 1) {
-          //       self.back()
-          //     } else {
-          //       if (res.isnew === 1) {
-          //         self.jump('/pages/account/companyUpdate/main?type=3')
-          //       } else {
-          //         self.tab('/pages/me/main')
-          //       }
-          //     }
-          //   }, 1500)
-          // }).catch(e => {
-          //   console.log('catch===>' + JSON.stringify(e))
-          //   self.jump('/pages/account/wxBind/main')
-          //   // modal提示是否绑定
-          // })
-        } else {
-          self.jump('/pages/account/wxBind/main')
+          me.login()
         }
       } else {
-        self.showMsg('点击过于频繁，请稍等！')
+        me.showMsg('点击过于频繁，请稍等！')
       }
+    },
+    login () {
+      let self = this
+      // 微信自动登录接口
+      let paramsObj = {
+        openId: self.openId || mpvue.getStorageSync('openId') || '',
+        unionId: self.unionId || mpvue.getStorageSync('unionId') || ''
+      }
+      self.httpGet(self.apiList.zf.wxLogin, paramsObj).then(res => {
+        self.setUser({token: res.data.token, user: res.data.user})
+        self.httpPost(self.apiList.zf.getPersonInfo, {}).then(res => {
+          self.setUser({user: res.data})
+          if (res.data.userStatus === '01') {
+            self.confirm({ content: '您是新用户，请先完善公司信息' }).then(res => {
+              if (res === 'confirm') {
+                self.jump('/pages/account/companyUpdate/main')
+              } else {
+                self.tab('/pages/me/main')
+              }
+            })
+          } else {
+            this.showMsg('登录成功')
+            setTimeout(function () {
+              self.canClick = true
+              if (self.backType === 1) {
+                console.log('+++++++aaa')
+                self.back()
+              } else {
+                console.log('+++++++bbb')
+                self.tab('/pages/index/main')
+              }
+            }, 500)
+          }
+        })
+      }).catch(e => {
+        console.log('微信未绑定过手机号，登陆失败——+++')
+        console.log(e)
+        self.jump('/pages/account/wxBind/main')
+      })
     }
   }
 }
