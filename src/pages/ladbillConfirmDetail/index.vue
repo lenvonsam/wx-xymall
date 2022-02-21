65d1d4667683ccf0d6ea92dc9d9ac7e3e75eea48
myforkwxd <1215368751@qq.com>
2022-02-21 11:02:02 +0800

    17814 【test】【型云商城】【型云小程序】型云合同开单后，里面含有卷类物资不允许做合同修改，需要在修改页面做出对应提示，确认修改按钮置灰

--- src/pages/ladbillConfirmDetail/index.vue
+++ src/pages/ladbillConfirmDetail/index.vue
@@ -57,6 +57,7 @@ div
     .footer.row.bg-white.text-center.text-white.padding-sm(:style="{'padding-bottom': isIpx ? '68rpx' : '20rpx'}")
      .bg-red.col.padding-sm.round(@click="updateBill('02')") 驳回
      .bg-blue.col.margin-left-sm.padding-sm.round(@click="updateBill('01')") 确认修改提单
+    alert(:msg="alertText", v-model="alertShow")
 </template>
 <script>
 import copyBtn from '@/components/CopyBtn.vue'
@@ -81,7 +82,10 @@ export default {
         hangingFee: 0,
         totalContract: 0,
         balancePayment: 0
-      }
+      },
+      alertShow: false,
+      isHasRoll: false,
+      alertText: '此合同含有卷类物资，不能进行修改，如需修改请联系业务员!'
     }
   },
   components: {
@@ -123,6 +127,10 @@ export default {
 
         ic.secondNumber = ic.pendingAmount
         ic.ratioAvailableAmount = ic.ratioAvailableAmount + ic.amount
+        if (ic.productBrandName.includes('卷')) {
+          this.alertShow = true
+          this.isHasRoll = true
+        }
       })
 
       // 旧单信息
@@ -196,6 +204,10 @@ export default {
   },
   methods: {
     updateBill (affirmStatus) {
+      if (this.isHasRoll) {
+        this.alertShow = true
+        return
+      }
       this.confirm({content: '请确认修改合同'}).then(res => {
         if (res === 'confirm') {
           this.showLoading()
